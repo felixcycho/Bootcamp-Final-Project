860315bb9d188d05c1a8ad9d7368acaae18cebf7
felixcycho <felixcycho@yahoo.com.hk>
2025-10-23 16:39:34 +0800

    felix

--- project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/contoller/StockDataAppOperation.java
+++ project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/contoller/StockDataAppOperation.java
@@ -1,9 +1,8 @@
 package com.bootcamp.demo.project_data_provider.contoller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import com.bootcamp.demo.project_data_provider.model.dto.QuoteDto;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import com.bootcamp.demo.project_data_provider.dto.QuoteDto;
 
 public interface StockDataAppOperation {
   @GetMapping(value = "/quote")
--- project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/finnhub/model/dto/QuoteDTO.java
+++ project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/finnhub/model/dto/QuoteDTO.java
@@ -1,8 +1,16 @@
 package com.bootcamp.demo.project_data_provider.finnhub.model.dto;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
+@Getter
+@Builder
 public class QuoteDTO {
-  
+  private String symbol;
+  private Double price; 
+  private Double dayHigh;
+  private Double dayLow;
+  private Double dayOpen;
+  private LocalDateTime datetime = LocalDateTime.now();
 }
