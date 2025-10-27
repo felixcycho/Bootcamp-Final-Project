756f31a936efd64428334baa90ee276393a260e9
felixcycho <felixcycho@yahoo.com.hk>
2025-10-27 16:50:51 +0800

    felix

--- project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/dto/CompanyProfileDto.java
+++ project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/dto/CompanyProfileDto.java
@@ -1,5 +1,6 @@
 package com.bootcamp.demo.project_data_provider.dto;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -12,4 +13,5 @@ public class CompanyProfileDto {
   private String stockName;
   private String ticker;
   private String mainIndustry;
+  private LocalDateTime datetime;
 }
--- project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/model/dto/CompanyProfileDTO.java
+++ project-data-provider/src/main/java/com/bootcamp/demo/project_data_provider/model/dto/CompanyProfileDTO.java
@@ -1,6 +1,9 @@
 package com.bootcamp.demo.project_data_provider.model.dto;
 
+import java.time.LocalDateTime;
+import com.bootcamp.demo.project_data_provider.config.UnixTimestampDeserializer;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.Getter;
 
 @Getter
@@ -13,4 +16,6 @@ public class CompanyProfileDTO {
   private String ticker;
   @JsonProperty (value = "finnhubIndustry")
   private String mainIndustry;
+  @JsonDeserialize(using = UnixTimestampDeserializer.class)
+  private LocalDateTime datetime = LocalDateTime.now();
 }
